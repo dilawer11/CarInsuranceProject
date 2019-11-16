6e550d6cb32f37fb84387e5c09708fc4b54f6f8d
Dilawer Ahmed <dilawer11@gmail.com>
2019-11-17 01:37:45 +0500

    Add Changes To Inspector

--- src/com/carinsurance/claim/Inspector.java
+++ src/com/carinsurance/claim/Inspector.java
@@ -6,6 +6,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Inspector {
+	class AccidentDetails {
+		public String accidentID;
+		public String type;
+		public String city;
+		public String date;
+	}
 	public int submitInitialReport(String policyID, String inspectorID, int carCondition, String comments) {
 		try{
 			Connection con = getConnection();
@@ -38,32 +44,29 @@ public class Inspector {
 		}
 	}
 //	TODO Change return type
-	public int getAccidentDetails(String accidentID) {
+	public ArrayList<AccidentDetails> getAccidentDetails(String accidentID) {
 		try{
 			Connection con = getConnection();
-			PreparedStatement statement = con.prepareStatement("SELECT * FROM Accident");
+			PreparedStatement statement = con.prepareStatement("SELECT * FROM Accident WHERE accidentID = ?");
 //			TODO Update Query
 			statement.setString(1, accidentID);
-			class AccidentDetails {
-				public String accidentID;
-				public String type;
-				public String city;
-				public String date;
-			}
-			
-			AccidentDetails ac;
-			ac.accidentID = "1";
-			ac.city = "Lahore";
-			ac.date = "2009-10-12";
-			ac.
+			ArrayList<AccidentDetails> resultList;
+			int index = 0;
 			ResultSet rs = statement.executeQuery();
 			while(rs.next()) {
-				
+				AccidentDetails ac;
+				ac.accidentID = rs.getString("accidentID");
+				ac.city = rs.getString("city");
+				ac.date = rs.getDate("date");
+				ac.type = rs.getString("type");
+				resultList.add(index, ac);
+				index++;
 			}
-			return 1;
+			return resultList;
 		} catch(Exception e){
 			System.out.println(e);
-			return -1;
+			ArrayList<AccidentDetails> resultList;
+			return resultList;
 		}
 	}
 //	TODO Change return type
@@ -98,6 +101,14 @@ public class Inspector {
 	}
 //	TODO change return type
 	public int getAssignedInspections(String inspectorID) {
+		Connection con = getConnection();
+		PreparedStatement initialReportStatement =  con.prepareStatement("SELECT * FROM InitialReport WHERE inspectorID = ? AND status ='PEND_INS'"); //TODO Check this Update for all other status
+		PreparedStatement accidentReportStatement = con.prepareStatement("SELECT * FROM AccidentReport WHERE inspectorID = ? AND status = 'PEND_INS'");
+		initialReportStatement.setString(1, inspectorID);
+		accidentReportStatement.setString(1, inspectorID);
+		ResultSet initialReportResultSet = executeQuery();
+		ResultSet accidentReportResultSet = executeQuery();
+		//TODO Compile into a result and send back
 		return 0;
 	}
 	private static Connection getConnection() throws Exception {
